6249cf380e43afcb745632a3d05d37e42be6fae6
min <145747769+marinmp3@users.noreply.github.com>
2023-10-24 09:48:10 +0200

    Delete random.choice.py

--- random.choice.py
+++
@@ -1,7 +0,0 @@
-import random
-creamer = input("Do you like ice cream?")
-def IcecreamFlavor():
-    flavor = ["mint", "chocolate", "vanilla", "strawberry"]
-    chooseFlavor = random.choice(flavor)
-    return f"Here you have {chooseFlavor} ice cream!"
-print (IcecreamFlavor())
