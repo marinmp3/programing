c7ddacc7d9423f85a2b8a2e9b15c0f60a5be7925
min <145747769+marinmp3@users.noreply.github.com>
2023-11-20 14:27:53 +0100

    Add files via upload

---
+++ matrices.py
@@ -0,0 +1,23 @@
+lista = [1,2,3]
+print (f"imprimo la lista: {lista}")
+print(f"Imprimo el elemento 2 de la lista: {lista[1]}")
+
+matriz = [[1,2,3],[4,5,6]]
+print (f"Imprimo la matriz: {matriz}")
+print (f"imprimo el elemento 2 de la lista: {matriz[1]}")
+print(f"Imprimo el elemento [2,1] de la lista de matrices: {matriz[1][0]}") #del segundo elemento (lista2) quiero el primer elemento
+
+#EJ1:
+# Imprimir por filas la matriz. 1 2 3
+#                               4 5 6
+#                               7 8 9
+
+matriz1 = [[1,2,3],[4,5,6],[7,8,9]]
+print(f"Imprimo la matriz en filas: {matriz1}")
+
+# Imprimir por columnas la matriz. 1 4 7
+#                                  2 5 8
+#                                  3 6 9
+
+matriz2 = [[1,4,7],[2,5,8],[3,6,9]]
+print(f"Imprimo la matriz en columnas: {matriz2}")
