5bf6bd53fa77a25d01aac117209148a386c81b7a
min <145747769+marinmp3@users.noreply.github.com>
2024-01-28 19:06:07 +0100

    Add files via upload

---
+++ EjerciciosBásicos2.java
@@ -0,0 +1,138 @@
+import java.util.Scanner;
+public class EjerciciosBásicos2 {
+    public static void main (String[] args){
+        // Numero natural: Solicita un número, si es menor que 0 imprime que no es un número natural.
+        Scanner sc = new Scanner(System.in);
+        // int numero;
+        // System.out.println("introduce un número: ");
+        // numero = sc.nextInt();
+        // if (numero < 0){
+        //     System.out.println("NO es un número natural");
+        // } else {
+        //     System.out.println("ES un número natural");
+        // }
+
+        // // .- Hora: Solicita una hora (hora,minutos,segundos) en una única línea, separada por espacios. Si los datos son incorrectos, lanza un mensaje de error.
+        // System.out.println("Introduce una hora [hora:minutos:segundos]: ");
+        // int hora = Integer.parseInt(args[0]);
+        // int minutos = Integer.parseInt(args[1]);
+        // int segundos = Integer.parseInt(args[2]);
+        // if (args.length == 3){
+        //     System.out.println("Elegiste la hora: " + hora + ":" + minutos + ":" + segundos);
+        // } else {
+        //     System.out.println("---ERROR---");
+        // }
+
+        // // .- Par: Solicita un número par, si el número introducido no es un número par lanza un mensaje de error.
+        // System.out.println("Introduce un número par: ");
+        // int numPar;
+        // numPar = sc.nextInt();
+        // if (numPar%2 != 0){
+        //     System.out.println(numPar + " no es un número par");
+        // } else {
+        //     System.out.println(numPar + " es un número par");
+        // }
+        // // .- Pasar a par: Solicita un número par, si el número introducido es impar súmale uno.
+        // System.out.println("Introduce un número impar para pasarlo a par: ");
+        // int numImpar;
+        // numImpar = sc.nextInt();
+        // if (numImpar%2 != 0){
+        //     System.out.println(numImpar + " 1" + " = " + numImpar+1 + " ,ahora es par");
+        // } else {
+        //     System.out.println(numImpar + " es un número par");
+        // }
+       // .- Pasar a positivo: Solicita un número positivo, si el número introducido es negativo pásalo a positivo.
+        // System.out.println("Introduce un número positivo: ");
+        // int numPositivo;
+        // int numAleatorio;
+        // numAleatorio = (int)(Math.random()*100)+1;
+        // numPositivo = sc.nextInt();
+        // if (numPositivo < 0){
+        //     System.out.println((numPositivo-numPositivo+numAleatorio)+ " | " + numPositivo + " ahora es un número positivo");
+        // } else {
+        //     System.out.println(numPositivo + " es un número positivo");
+        // }
+        // .- Comprendido entre: Solicita una número y asegúrate de que está comprendida entre otros dos números introducidos, 
+        // si el número introducido no cumple la condición, lanza un mensaje de error.
+        // int num1;
+        // int num2;
+        // int numIntro;
+        // System.out.println("Establece el valor mínimo: ");
+        // num1 = sc.nextInt();
+        // System.out.println("Establece el valor máximo: ");
+        // num2 = sc.nextInt();
+        // System.out.println("Introduce un número: ");
+        // numIntro = sc.nextInt();
+        // if (numIntro < num1 || numIntro > num2){
+        //     System.out.println("El número " + numIntro + " no está entre el rango establecido" + "(" + num2 + "|" + num1 + ")");
+        // } else {
+        //     System.out.println(numIntro + " está en el rango");
+        // }
+        // .- Rango de edad: Solicita una edad y asegúrate de que está comprendida entre 0 y 120 años,
+        // si el número introducido no cumple la condición, lanza un mensaje de error.
+            // System.out.println("Introduce un número -- rango (0-120): ");
+            // int minimo = 0;
+            // int maximo = 120;
+            // int numIntro;
+            // numIntro = sc.nextInt();
+            // if (numIntro < minimo || numIntro > maximo){
+            //     System.out.println("El número " + numIntro + " no está en el rango establecido" + "(" + maximo + "|" + minimo + ")");
+            // } else {
+            //     System.out.println(numIntro + " está en el rango");
+            // }
+
+            // .- Vocal: Solicita una vocal, si dato introducido no es una vocal, lanza un mensaje de error. [!] Ascii.
+            // System.out.println("Introduce una vocal:  ");
+            // String vocal;
+            // vocal = sc.nextLine();
+            // switch (vocal) {
+            //     case "a":
+            //     case "e":
+            //     case "i":
+            //     case "u":
+            //     case "o":
+            //         System.out.println("Es una vocal");
+            //         break;
+            //     default:
+            //         System.out.println("No es una vocal");
+            // }
+            // .- Letra: Solicita una letra, si dato introducido no es una letra, lanza un mensaje de error.
+            // System.out.println("Introduce una letra (minúscula):  ");
+            // String letraA = "a";
+            // String letraZ = "z";
+            // String letra;
+            // letra = sc.nextLine();
+            // if (letra != letraA : letraZ){
+            //     System.out.println("No es una letra");
+                
+            // }else {
+            //     System.out.println("Es una letra");
+                
+            // } 
+            // .- Letra: Solicita una letra mayúsucula, si dato introducido no es una letra mayúscula, lanza un mensaje de error.
+
+            // .- Pasar a mayúscula: Solicita una letra mayúscula, si dato introducido no es una letra mayúscula,
+            //  pero si una letra minúscula, pasalá a mayúscula.
+            System.out.println("Introduce una letra mayúscula: ");
+            String letraMayús;
+            char letra = letraMayús.charAt(0);
+            letraMayús = sc.nextLine();
+            if (letra != "A"){ 
+
+            }
+
+
+        }
+        // .- Dígito: Solicita un dígito, si dato introducido no es un dígito, lanza un mensaje de error.
+
+    }
+    
+
+
+
+
+
+
+
+
+
